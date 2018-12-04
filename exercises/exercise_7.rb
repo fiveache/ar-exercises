cbee41bba7d6e4e06a5903724bbc086a23d25350
David Lacho <david.lacho@gmail.com>
2018-12-04 21:56:49 +0000

    finished exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,19 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
+puts "what's a name for a store?"
+new_store_name = gets.chomp
+
+@users_store = Store.create(
+  name: new_store_name
+)
 # Your code goes here ...
+
+@users_store.errors.full_messages.each do |message|
+  puts message
+end
+
+# binding.pry
