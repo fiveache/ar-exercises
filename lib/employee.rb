1f0c7ba627ac4b16d64bfd6a01f2004b21246cb2
David Lacho <david.lacho@gmail.com>
2018-12-05 01:14:07 +0000

    put in private

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,4 +3,11 @@ class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  before_create :assign_password, on: :create
+
+  private
+
+  def assign_password
+    self.password = rand 10_000_000..99_999_999
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -4,9 +4,21 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :must_carry_women_or_men
 
+  before_destroy :checkIfEmpty
+
+
+  private
+
   def must_carry_women_or_men
-    if !mens_apparel && !womens_apparel
+    if mens_apparel == false && womens_apparel == false
       errors.add(:base, 'Must carry gendered apparel.')
+    end
+  end
+
+  def checkIfEmpty
+    if employees.size >= 1
+      errors.add(:base, 'This store has employees in it.')
+      throw :abort
     end
   end
 end
